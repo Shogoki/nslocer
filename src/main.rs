1e00b2acfe654942bfdb882a7ffb8f585b7addac
Fran Rimoldi <francisco.rimoldi@gmail.com>
2024-02-26 22:14:46 -0300

    refactor main fn

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,16 @@ use std::process;
 use clap::Parser;
 use nslocer::Args;
 
-fn main() {
+fn main() -> serde_json::Result<()> {
     let args = Args::parse();
 
     match nslocer::run(args) {
-        Ok(metrics) => match serde_json::to_string(&metrics) {
-            Ok(json) => println!("{:#?}", json),
-            Err(e) => {
-                println!("Error parsing metrics");
-                println!("{}", e);
-                process::exit(1)
-            }
-        },
+        Ok(metrics) => {
+            let json = serde_json::to_string(&metrics)?;
+            println!("{}", json);
+
+            Ok(())
+        }
         Err(e) => {
             println!("Analyzer couldn't get metrics");
             println!("{}", e);
